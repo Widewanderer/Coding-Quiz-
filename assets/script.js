336a6dfe79d293abc585ba6454133d3e1836425e
Tyson Trotz <slavatrotz@gmail.com>
2023-07-22 22:29:03 -0500

    changed background to dark grey, text green, added borders, margins, button, tweeked Ids/classes

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var questions = document.querySelector("main-text");
+// change "title to questions "
+
+questions.textContent = "Hello Slava";
+
+// DOM manipulation
+// timer for countdown clock with event (loose 5 seconds for incorect answer)
+// local storage for high scores
